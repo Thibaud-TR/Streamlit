2fd06b265173c3b90d1595fe3071403159207414
root <root@DESKTOP-H9UOHHA.localdomain>
2024-12-04 12:33:31 +0100

    MAJ SCRPIT

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
+from streamlit_option_menu import option_menu
 
 # Nos données utilisateurs doivent respecter ce format
 
@@ -23,35 +24,48 @@ authenticator = Authenticate(
     30, # Le nombre de jours avant que le cookie expire 
 )
 
-authenticator.login()
 
 def accueil():
-      st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
 
+    st.title("Bienvenue sur la page d'accueil")
+    st.image('https://img.freepik.com/photos-gratuite/mot-bienvenue-disponible-lancement-ouvert_53876-124476.jpg?semt=ais_hybrid')
+    
+def photos():
+
+    st.title("Bienvenue sur la page photos")
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("Panda 1")
+        st.image("https://img.freepik.com/photos-gratuite/vue-ours-panda-dans-nature_23-2150453056.jpg?ga=GA1.1.1585893942.1733311562&semt=ais_hybrid")
+
+    with col2:
+        st.header("Panda 2")
+        st.image("https://img.freepik.com/photos-gratuite/ours-panda-geant-allonge-dos-mangeant-pousses-bambou_493961-11.jpg?t=st=1733311784~exp=1733315384~hmac=1ab3a86de8ec6f2ffd93e6fdeaff69377d56580b974f3fb8da452144924f1440&w=740")
+
+    with col3:
+        st.header("Panda 3")
+        st.image("https://img.freepik.com/photos-gratuite/vue-rapprochee-animal-feutre_23-2151728573.jpg?t=st=1733311847~exp=1733315447~hmac=59594370bbaae6e5edf0dfbad5f839360b1b6d126df9508ef8e7196b27790527&w=740")
+
+authenticator.login()
 
 if st.session_state["authentication_status"]:
-  accueil()
-  # Le bouton de déconnexion
-  authenticator.logout("Déconnexion")
+    with st.sidebar :
+        authenticator.logout("Déconnexion")
+        selection = option_menu( menu_title = 'Navigation', options = ["Accueil", "Photos"], icons=['house', 'journal'])
 
 elif st.session_state["authentication_status"] is False:
     st.error("L'username ou le password est/sont incorrect")
+    selection = 'connec'
 elif st.session_state["authentication_status"] is None:
     st.warning('Les champs username et mot de passe doivent être remplie')
+    selection = 'connec'
 
 
-# Importation du module
-from streamlit_option_menu import option_menu
-
-# Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
-
-# On indique au programme quoi faire en fonction du choix
-if selection == "Accueil":
-    st.write("Bienvenue sur la page d'accueil !")
+if selection == 'connec' :
+    st.title("Saisir vos identifiants et mot de passe")
+elif selection == "Accueil":
+    accueil()
 elif selection == "Photos":
-    st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
+    photos()
